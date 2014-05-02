48a1b07143abe92364d188e95259ceffb7c44908
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-05-02 14:04:55 -0400

    Remove ember global jshint line

--- model/templates/base.js
+++ model/templates/base.js
@@ -1,4 +1,3 @@
-/*global Ember*/
 <%= _.classify(appname) %>.<%= _.classify(name) %> = DS.Model.extend({<% _.each(attrs, function(attr, i) { %>
     <%= _.camelize(attr.name) %>: DS.attr('<%= attr.type %>')<% if(i < (attributes.length - 1)) { %>,<% } %>
 <% }); %>});
