17435144480b9dca057b02e7f2871606042a6697
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-07-31 13:13:16 -0400

    OCD use single quotes for `defaultLocale`

--- app/templates/scripts/l10n/translations.js
+++ app/templates/scripts/l10n/translations.js
@@ -1,6 +1,6 @@
 // If fallbacks is set to `true`, this is the language that will be used if
 // the current language has no available translation.
-I18n.defaultLocale = "en-US";
+I18n.defaultLocale = 'en-US';
 I18n.locale = window.navigator.language || 'en-US';
 // Set to false to show missing translations.
 I18n.fallbacks = true;
